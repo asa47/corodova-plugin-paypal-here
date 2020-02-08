1e0127e60e2a4221e5b4dfc1a9980816e464101c
Russell <asa47@hotmail.it>
2020-02-08 10:40:14 +0000

    Delete CDVPayPalHere-Bridging-Header.h

--- src/ios/CDVPayPalHere-Bridging-Header.h
+++
@@ -1,5 +0,0 @@
-// #import <SimpleKeychain/SimpleKeychain.h>
-// #import <PPRetailInstrumentInterface/PPRetailInstrumentInterface.h>
-// #import <Trustkit/Trustkit.h>
-// #import <AFNetworking/AFNetworking.h>
-// #import <PayPalRetailSDK/PayPalRetailSDK.h>
